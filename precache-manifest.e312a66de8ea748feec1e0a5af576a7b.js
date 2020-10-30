d0382780106d273be69f118512903145aab5928e
Ohad Leshem <ohadlesh@gmail.com>
2020-10-30 16:02:02 +0200

    Updates

---
+++ precache-manifest.e312a66de8ea748feec1e0a5af576a7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "257bf5985a5b5a0a35b49f6875b9cb0e",
+    "url": "/Order-Up/index.html"
+  },
+  {
+    "revision": "9c37a8a94b1f88699991",
+    "url": "/Order-Up/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "5ce01f21b76de36d7adc",
+    "url": "/Order-Up/static/css/main.6acbac95.chunk.css"
+  },
+  {
+    "revision": "9c37a8a94b1f88699991",
+    "url": "/Order-Up/static/js/2.651ea0c3.chunk.js"
+  },
+  {
+    "revision": "78fd0f62364854e39c53711d027f73a3",
+    "url": "/Order-Up/static/js/2.651ea0c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5ce01f21b76de36d7adc",
+    "url": "/Order-Up/static/js/main.9a77f273.chunk.js"
+  },
+  {
+    "revision": "1219eda4ba80753d5ff9",
+    "url": "/Order-Up/static/js/runtime-main.85ffce54.js"
+  }
+]);
